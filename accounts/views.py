cf88fe0c0e11ea142449f1ca88dbdc95c328f2d5
rahulgiri2019 <rahulgiri201486@gmail.com>
2019-09-16 19:12:18 +0530

    initial commit

---
+++ accounts/views.py
@@ -0,0 +1,28 @@
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import login as auth_login
+from django.contrib.auth.models import User
+from django.shortcuts import render, redirect
+from django.urls import reverse_lazy
+from django.views.generic import UpdateView
+
+from .forms import SignUpForm
+
+def signup(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            auth_login(request, user)
+            return redirect('home')
+    else:
+        form = SignUpForm()
+    return render(request, 'signup.html', {'form': form})
+
+class UserUpdateView(UpdateView):
+    model = User
+    fields = ('first_name','last_name','email',)
+    template_name = 'my_account.html'
+    success_url = reverse_lazy('my_account')
+
+    def get_object(self):
+        return self.request.user
